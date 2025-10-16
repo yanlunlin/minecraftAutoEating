761f2ad22ba95412c1d125cd641880f05a199b69
yenlunlin <yanlunlin@github.com>
2025-10-16 20:29:22 +0800

    add output cature

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import numpy as np
 import mss
 import cv2
 import time
+import os 
 
 TOP = 919
 LEFT = 997
 WIDTH = 69
 HEIGHT = 47
+
+SAVE_FOLDER = "picture"
 
 monitor = {
     "top": TOP,
@@ -19,6 +22,10 @@ sct = mss.mss()
 
 print("程式執行中")
 
+if not os.path.exists(SAVE_FOLDER):
+    os.makedirs(SAVE_FOLDER)
+
+frame_count = 0
 while True:
     start_time = time.time()
     sct_img = sct.grab(monitor)
@@ -29,6 +36,11 @@ while True:
     cv2.imshow("遊戲截圖（按 q 退出）", frame)
 
     fps = 1 / (time.time() - start_time)
+    frame_count += 1
+    if not frame % 100:
+        filename = os.path.join(SAVE_FOLDER, f"capture_{frame_count}.jpg")
+        cv2.imwrite(filename, frame)
+        print("儲存圖片")
     print(f"FPS: {fps:.2f}", end="\r")
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
